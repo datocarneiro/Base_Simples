b6853e5fe47e6b1a21b8f6d1622de8112baf0ed8
datocarneiro <132966071+datocarneiro@users.noreply.github.com>
2024-02-02 10:01:01 -0300

    a

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-elitefc = Flask(__name__)
+app = Flask(__name__)
 
-@elitefc.route('/')
+@app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    elitefc.run(debug=True)
+    app.run(debug=True)
